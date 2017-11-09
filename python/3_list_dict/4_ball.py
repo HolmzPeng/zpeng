9e3337c3c186360065696a3ae226944d6fd4c637
kyo <iprintf@qq.com>
2017-11-09 11:20:35 +0800

    完善弹球程序
    完善反弹效果
    增加控制弹球颜色
    增加控制弹球方向
    增加随机障碍物并且碰到退出程序

--- python/3_list_dict/4_ball.py
+++ python/3_list_dict/4_ball.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 import gt
+import os
+from random import randint
 
-W, H = 60, 20
+W, H = 20, 10
 r, c = 3, 3
+fr, fc, fn, ft = randint(2, H - 1), randint(2, W - 1), 1, randint(10, 50)
+r_inc, c_inc = 1, 1
+color = 31
 quit = False
 
+def ballExit():
+    global quit
+
+    quit = True
+    gt.exit()
+    print("\033[?25h\033[%d;1H" % (H + 2))
+    os._exit(0)
+
 def ball():
-    global r, c
+    global r, c, r_inc, c_inc, color, fc, fr, fn, ft, quit
 
     while not quit:
         print("\033[1;1H", end="", flush=True)
@@ -19,18 +32,36 @@ def ball():
                     print(" ", end="")
             print()
 
-        print("\033[%d;%dH@" % (r, c), end="", flush=True)
+        fn += 1
+        #  if fn == ft:
+            #  fr, fc, fn, ft = (randint(2, H - 1), randint(2, W - 1),
+                                #  1, randint(10, 50))
+        print("\033[%d;%dH#" % (fr, fc), end="", flush=True)
 
-        if r == H:
-            r -= 1
-        else:
-            r += 1
+        print("\033[%d;%dH\033[%dm@\033[0m" % (r, c, color),
+                    end="", flush=True)
+
+        if r == fr and c == fc:
+            break
+
+        if r + r_inc < 1 or r + r_inc > H:
+            r_inc = -r_inc
+
+        if c + c_inc < 1 or c + c_inc > W:
+            c_inc = -c_inc
+
+        #  color = randint(30, 37)
+
+        r += r_inc
+        c += c_inc
 
         gt.sleep(0.1)
 
+    ballExit()
+
 
 def main():
-    global quit
+    global quit, color, r_inc, c_inc
 
     gt.init()
 
@@ -38,15 +69,22 @@ def main():
 
     gt.run(ball)
 
-    while True:
+    while not quit:
         ch = gt.get()
         if ch == 'q':
             break
+        elif ch == ' ':
+            color = randint(30, 37)
+        elif ch == 'w':
+            r_inc = -1
+        elif ch == 's':
+            r_inc = 1
+        elif ch == 'a':
+            c_inc = -1
+        elif ch == 'd':
+            c_inc = 1
 
-    quit = True
-
-    print("\033[?25h")
-    gt.exit()
+    ballExit()
 
 if __name__ == "__main__":
     main()
