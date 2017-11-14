4f528ea5baddc77a557b5baf1cbe775fd2858d33
kyo <iprintf@qq.com>
2017-11-14 12:01:38 +0800

    扩展弹球程序代码使之更通用
    改变弹球程序核心算法,　围绕二维列表实现动画效果

---
+++ python/3_list_dict/7_ball_dict_list.py
@@ -0,0 +1,249 @@
+#!/usr/bin/env python3
+
+import gt
+import sys
+import os
+import vt
+from random import randint
+
+def createBall(win, r=None, c=None, w=None, h=None, r_inc=None, c_inc=None,
+                color=None, ch=None):
+    """
+    创建一个弹球对象并且初始化数据
+    """
+    ball = {}
+    ball['r'] = r if r is not None else randint(1, win['height'] - 2)
+    ball['c'] = c if c is not None else randint(1, win['width'] - 2)
+    ball['w'] = w if w is not None else randint(1, 5)
+    ball['h'] = h if h is not None else randint(1, 5)
+    ball['r_inc'] = r_inc if r_inc is not None else (-1, 1)[randint(0, 1)]
+    ball['c_inc'] = c_inc if c_inc is not None else (-1, 1)[randint(0, 1)]
+    ball['color'] = color if color is not None else vt.getColor()
+    ball['ch'] = ch if ch is not None else "@*$%ABCDEF"[randint(0, 9)]
+
+    return ball
+
+
+def addBall(win, num=1, balls=None):
+    """
+    添加多个球, 返回保存所有球数据的列表
+    """
+    if balls is None:
+        balls = []
+
+    for i in range(num):
+        balls.append(createBall(win))
+
+    return balls
+
+def randObstacle(win, obs=None):
+    """
+    随机一个障碍物
+    """
+    if obs is None:
+        obs = {}
+    obs['r'] = randint(1, win['height'] - 2)
+    obs['c'] = randint(1, win['width'] - 2)
+    obs['w'] = randint(1, 5)
+    obs['h'] = randint(1, 5)
+    obs['color'] = vt.getColor()
+    obs['ch'] = '#'
+    obs['count'] = 0
+    obs['total'] = randint(10, 50)
+
+    return obs
+
+def addObstacles(win, num=1, obstacles=None):
+    """
+    添加多个障碍物对象及初始化数据
+    """
+    if obstacles is None:
+        obstacles = []
+
+    for i in range(num):
+        obstacles.append(randObstacle(win))
+
+    return obstacles
+
+
+def ballExit(win):
+    """
+    弹球程序退出处理函数
+    """
+    gt.exit()
+    vt.run([vt.SHOW, vt.goto(win['height'] + 1)])
+    os._exit(0)
+
+
+def ballInit(w=30, h=10, ballNum=1, obsNum=1):
+    """
+    弹球程序的初始化
+    """
+    gt.init()
+    vt.run([vt.HIDE, vt.SCREEN, vt.goto()])
+
+    win = {}
+    win['width'] = w
+    win['height'] = h
+    win['convas'] = [[' '] * w for x in range(h)]
+
+    ballPro = {}
+    ballPro['pause'] = False
+    ballPro['win'] = win
+    ballPro['balls'] = addBall(ballPro['win'], ballNum)
+    ballPro['obstacles'] = addObstacles(ballPro['win'], obsNum)
+
+    return ballPro
+
+
+def convasInit(width, height, convas):
+    """
+    画布初始化
+    """
+    for i in range(height):
+        for j in range(width):
+            if i == 0 or j == 0 or i == height - 1 or j == width - 1:
+                convas[i][j] = '#'
+            else:
+                convas[i][j] = ' '
+
+    #  showBall(convas)   #测试使用
+
+
+def drawObj(win, objs):
+    """
+    画障碍物或弹球
+    """
+    def _drawObj(win, o):
+        convas = win['convas']
+
+        for i in range(o['h']):
+            for j in range(o['w']):
+                r, c = o['r'] + i, o['c'] + j
+
+                if (r < 0 or r > win['height'] - 1
+                        or c < 0 or c > win['width'] - 1):
+                    continue
+
+                convas[r][c] = o
+
+    for o in objs:
+        _drawObj(win, o)
+
+
+def moveBall(win, ball, obstacles):
+    """
+    移动弹球
+    """
+    if (ball['r'] + ball['r_inc'] < 0
+            or ball['r'] + ball['r_inc'] + ball['h'] >= win['height']):
+        ball['r_inc'] = -ball['r_inc']
+
+    if (ball['c'] + ball['c_inc'] < 0
+            or ball['c'] + ball['c_inc'] + ball['w'] >= win['width']):
+        ball['c_inc'] = -ball['c_inc']
+
+    for o in obstacles:
+        if (o['r'] <= ball['r'] <= o['r'] + o['h']
+                and o['c'] <= ball['c'] <= o['c'] + o['w']):
+            ball['r_inc'] = -ball['r_inc']
+            ball['c_inc'] = -ball['c_inc']
+            #  ballExit(win)
+
+    ball['r'] += ball['r_inc']
+    ball['c'] += ball['c_inc']
+
+
+def moveObstacles(win, obs):
+    """
+    障碍物随机
+    """
+    obs['count'] += 1
+    if obs['count'] == obs['total']:
+        randObstacle(win, obs)
+
+
+def showBall(convas):
+    """
+    刷新画布数据到屏幕
+    """
+    vt.goto(back=False)
+    for r in convas:
+        for o in r:
+            if type(o) == dict:
+                vt.color(o['ch'], o['color'], back=False)
+            else:
+                print(o, end='')
+        print()
+
+
+def ballRun(ballPro):
+    """
+    弹球程序动画函数
+    """
+    while True:
+        if not ballPro['pause']:
+            convasInit(**ballPro['win'])
+            drawObj(ballPro['win'], ballPro['obstacles'])
+            drawObj(ballPro['win'], ballPro['balls'])
+
+            #  for obs in ballPro['obstacles']:
+                #  moveObstacles(ballPro['win'], obs)
+
+            for ball in ballPro['balls']:
+                moveBall(ballPro['win'], ball, ballPro['obstacles'])
+
+            showBall(ballPro['win']['convas'])
+
+            #  调试信息
+            #  vt.run([vt.SAVE, vt.goto(ballPro['win']['height'] + 2), vt.LINE,
+                    #  vt.color(ballPro['balls'], vt.RED), vt.LOAD])
+
+        gt.sleep(0.1)
+
+
+def ballStart(ballPro):
+    """
+    弹球程序开始启动
+    """
+    gt.run(ballRun, args=(ballPro, ))
+
+    while True:
+        ch = gt.get()
+        if ch == 'q':
+            break
+        elif ch == chr(10):
+            ballPro['pause'] = not ballPro['pause']
+        elif ch == ' ':
+            for b in ballPro['balls']:
+                b['color'] = vt.getColor()
+        elif ch == 'w':
+            for b in ballPro['balls']:
+                b['r_inc'] = -1
+        elif ch == 's':
+            for b in ballPro['balls']:
+                b['r_inc'] = 1
+        elif ch == 'a':
+            for b in ballPro['balls']:
+                b['c_inc'] = -1
+        elif ch == 'd':
+            for b in ballPro['balls']:
+                b['c_inc'] = 1
+        elif ch == 'g':
+            for b in ballPro['balls']:
+                b['r_inc'] = [-1, 1][randint(0, 1)]
+                b['c_inc'] = [-1, 1][randint(0, 1)]
+
+    ballExit(ballPro['win'])
+
+
+if __name__ == "__main__":
+    #  print(sys.argv)
+    w = int(sys.argv[1]) if len(sys.argv) > 1 else 20
+    h = int(sys.argv[2]) if len(sys.argv) > 2 else 10
+    ballNum = int(sys.argv[3]) if len(sys.argv) > 3 else 1
+    obsNum = int(sys.argv[4]) if len(sys.argv) > 4 else 1
+
+    ballPro = ballInit(w, h, ballNum, obsNum)
+    ballStart(ballPro)
+
